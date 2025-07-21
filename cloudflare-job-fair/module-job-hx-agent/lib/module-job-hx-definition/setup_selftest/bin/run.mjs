a286ddd656a0f18b39a307646164bcb6e27675e6
Дід Alik <didalik.aim@gmail.com>
2025-07-21 22:14:00 +0000

    Run setup_selftest

--- cloudflare-job-fair/module-job-hx-agent/lib/module-job-hx-definition/setup_selftest/bin/run.mjs
+++ cloudflare-job-fair/module-job-hx-agent/lib/module-job-hx-definition/setup_selftest/bin/run.mjs
@@ -5,44 +5,46 @@ import { // {{{1
   loadKeys, secdVm,
 } from '../../../../../../public/lib/sdk.mjs' // FIXME
 import { 
-  HEX_FEE, 
+  HEX_FEE, makeOffer, makeRequest,
 } from '../../../../../../public/lib/api.mjs' // FIXME
 import * as fs from "node:fs"
-import { Asset, } from '@stellar/stellar-sdk'
-/*
-  f_add_hex_makes: async (node, run, job, nwdir, limit) => { // {{{2
-    let url = 'https://github.com/didalik/stellar-help-exchange/blob/main/README.md'
-    r = await makeRequest.call(vm, // = Port Angeles = {{{3
-      vm.d.agent, Keypair.fromSecret(vm.d.keysAgent[0]),
-      requestPARTNERS_WELCOME(url)   // Port Angeles //////
-    )
-    makeIds = makeIds + r.txId + ' '
-    log('- f_add_hex_makes makeRequest', r.request, r.amount, r.balanceId, r.txId)
+import { Asset, Keypair, } from '@stellar/stellar-sdk'
 
-    r = await makeRequest.call(vm, // = Tagoba = {{{3
-      vm.d.agent, Keypair.fromSecret(vm.d.keysAgent[0]),
-      requestHELP_WANTED(url)        // Tagoba ////////////
-    )
-    makeIds = makeIds + r.txId + ' '
-    log('- f_add_hex_makes makeRequest', r.request, r.amount, r.balanceId, r.txId)
-    
-    r = await makeOffer.call(vm, //   = Kyiv = {{{3
-      vm.d.agent, Keypair.fromSecret(vm.d.keysAgent[0]),
-      offerAIM_FOR_BUSINESSES(url)   // Kyiv //////////////
-    )
-    makeIds = makeIds + r.txId + ' '
-    log('- f_add_hex_makes makeOffer', r.offer, r.balanceId, r.txId)
+function offerAIM_FOR_BUSINESSES (url) { // {{{1
+  let hash = '#%D0%B4%D1%96%D0%B4-alik--the-kids-aim-for-businesses'
+  return `<b>Did Alik & the Kids: AIM FOR BUSINESSES</b><br/>
+Automated Integration Modeling by DA&K. Inquiry fee HEXA 10.
+<a href='${url}${hash}' target='_blank'>More...</a>`;
+}
 
-    r = await makeRequest.call(vm, // = Aukland = {{{3
-      vm.d.agent, Keypair.fromSecret(vm.d.keysAgent[0]),
-      requestAGENTS_WANTED(url)     // Aukland ///////////
-    )
-    makeIds = makeIds + r.txId // Look, Ma! NO SPACE!
-    log('- f_add_hex_makes makeRequest', r.request, r.amount, r.balanceId, r.txId)
-    // }}}3
-    fs.writeFileSync(nwdir + '/HEX_Agent_make2map.txids', makeIds)
-  },
-*/
+function offerHEX_FOR_SALE (url) { // {{{1
+  let hash = '#stellar-hex-for-sale'
+  return `<b>Stellar HEX: FOR SALE</b><br/>
+Full or partial ownership. Inquiry fee HEXA 100.
+<a href='${url}${hash}' target='_blank'>More...</a>`;
+}
+
+function requestAGENTS_WANTED (url) { // {{{1
+  let hash = '#stellar-hex-agents-wanted'
+  return `<b>Stellar HEX: AGENTS WANTED</b><br/>
+Signing bonus HEXA 10 + #(jobs run) * rate.
+<a href='${url}${hash}' target='_blank'>More...</a>`;
+}
+
+function requestHELP_WANTED (url) { // {{{1
+  let hash = '#stellar-hex-help-wanted'
+  return `<b>Stellar HEX: HELP WANTED</b><br/>
+Signing bonus HEXA 1000 + (job done) * (hourly rate).
+<a href='${url}${hash}' target='_blank'>More...</a>`;
+}
+
+function requestPARTNERS_WELCOME (url) { // {{{1
+  let hash = '#stellar-hex-partners-welcome'
+  return `<b>Stellar HEX: PARTNERS WELCOME</b><br/>
+Signing bonus HEXA 1000 + profit / 2.
+<a href='${url}${hash}' target='_blank'>More...</a>`;
+}
+
 async function setup_selftest (limit, nwdir) { // {{{1
   let agent_keys = nwdir + '/HEX_Agent.keys'
   let creator_keys = nwdir + '.keys'
@@ -53,7 +55,46 @@ async function setup_selftest (limit, nwdir) { // {{{1
     loadKeys(fs, agent_keys), // keysAgent
     log, limit, HEX_FEE, nwdir.endsWith('public'), null, fs
   )
-  console.log('setup_selftest nwdir', nwdir, 'vm', vm)
+  //console.log('setup_selftest nwdir', nwdir, 'vm', vm)
+
+  let url = 'https://github.com/didalik/stellar-help-exchange/blob/main/README.md'
+
+  let r = await makeOffer.call(vm,     // = Onna = {{{2
+    vm.d.agent, Keypair.fromSecret(vm.d.keysAgent[0]),
+    offerHEX_FOR_SALE(url)         // Onna //////////////
+  )
+  let makeIds = r.txId + ' '
+  log('- f_add_hex_makes makeOffer', r.offer, r.balanceId, r.txId)
+
+  r = await makeRequest.call(vm, // = Port Angeles = {{{2
+    vm.d.agent, Keypair.fromSecret(vm.d.keysAgent[0]),
+    requestPARTNERS_WELCOME(url)   // Port Angeles //////
+  )
+  makeIds = makeIds + r.txId + ' '
+  log('- f_add_hex_makes makeRequest', r.request, r.amount, r.balanceId, r.txId)
+
+  r = await makeRequest.call(vm, // = Tagoba = {{{2
+    vm.d.agent, Keypair.fromSecret(vm.d.keysAgent[0]),
+    requestHELP_WANTED(url)        // Tagoba ////////////
+  )
+  makeIds = makeIds + r.txId + ' '
+  log('- f_add_hex_makes makeRequest', r.request, r.amount, r.balanceId, r.txId)
+  
+  r = await makeOffer.call(vm, //   = Kyiv = {{{2
+    vm.d.agent, Keypair.fromSecret(vm.d.keysAgent[0]),
+    offerAIM_FOR_BUSINESSES(url)   // Kyiv //////////////
+  )
+  makeIds = makeIds + r.txId + ' '
+  log('- f_add_hex_makes makeOffer', r.offer, r.balanceId, r.txId)
+
+  r = await makeRequest.call(vm, // = Aukland = {{{2
+    vm.d.agent, Keypair.fromSecret(vm.d.keysAgent[0]),
+    requestAGENTS_WANTED(url)     // Aukland ///////////
+  )
+  makeIds = makeIds + r.txId // Look, Ma! NO SPACE!
+  log('- f_add_hex_makes makeRequest', r.request, r.amount, r.balanceId, r.txId)
+  // }}}2
+  fs.writeFileSync(nwdir + '/HEX_Agent_make2map.txids', makeIds)
 }
 
 await setup_selftest(process.argv[2], process.argv[3]).then(_ => process.exit(0)). // {{{1
